1738627e7dde8eac1166c0c921cc15b031e2edfb
harshitha93 <harshitha0193@gmail.com>
2017-06-05 12:56:43 +0530

    first commit for kotlin

---
+++ app/src/main/java/cherrywork/com/kotlintrial/FirstTrialKotlin.kt
@@ -0,0 +1,15 @@
+package cherrywork.com.kotlintrial
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Vikash Periwal on 19-05-2017.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
